12149c8ee9ce8f71c437dfc19608cc54a31776c5
Sohit-Giri <129953866+Sohit-Giri@users.noreply.github.com>
2024-09-21 21:44:40 +0545

    Add files via upload

--- sohit/settings.py
+++ sohit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!@=gtmztv-jkbl$8yi@mrd^!q2pbgs=h-1__uwr@rhsngg$lr$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
