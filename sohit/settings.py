a4852d4a19d3fc4fbd4dc950b47c64cebcf2970d
Sohit-Giri <129953866+Sohit-Giri@users.noreply.github.com>
2024-09-21 21:48:33 +0545

    Add files via upload

--- sohit/settings.py
+++ sohit/settings.py
@@ -76,14 +76,11 @@ WSGI_APPLICATION = "sohit.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'byb',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
